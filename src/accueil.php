2624e6b85bb746f3857705cc6a02bbb08e3542fd
julienx-hub <julien.272737@gmail.com>
2025-11-26 20:38:05 +0100

    Julien Mathias : Ajout des détections de l'utilisateur courant , création  d'une session , pour récupérer le login saisit par l'utilisateur grace à $_session

--- src/accueil.php
+++ src/accueil.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Gestion parc IT - Accueil</title>
@@ -10,41 +10,71 @@ session_start();
 </head>
 <body>
 
-
-
 <div class="wrapper">
     <header>
-    <h1>Plateforme de gestion du parc informatique</h1>
-    <a href="pages/connexion.php">Connexion</a>
+        <h1>Plateforme de gestion du parc informatique</h1>
+
+        <?php
+        if (isset($_SESSION['login'])) {
+            echo '<p class="user-info">Bienvenue ' .$_SESSION['login']. '</p>';
+            echo '<a class="btn-deco" href="pages/logout.php">Déconnexion</a>';
+        }
+        else {
+            echo '<a class="btn-connexion" href="pages/connexion.php">Connexion</a>';
+        }
+        ?>
     </header>
+
     <div class="main-container">
         <nav>
             <ul>
-                <li><a href="accueil.html">Page principale</a></li>
-                <li><a href="pages/inventaire.php">Inventaire</a></li>
-                <li><a href="pages/gestion.php">Gestion</a></li>
-            </ul>
+                <?php
+                if (!isset($_SESSION['login'])) {
+                    echo '<li><a href="accueil.php">Page principale</a></li>';
+                }
 
+                //client
+                if (isset($_SESSION['login']) && $_SESSION['login'] === 'client') {
+                    echo '<li><a href="accueil.php">Page principale</a></li>
+                          <li><a href="pages/inventaire.php">Inventaire</a></li>';
+                }
+
+                // admin / tech / sysadmin
+                if (isset($_SESSION['login']) &&
+                        ($_SESSION['login'] === 'adminweb' ||
+                                $_SESSION['login'] === 'sysadmin' ||
+                                $_SESSION['login'] === 'tech1')) {
+
+                    echo '<li><a href="accueil.php">Page principale</a></li>
+                          <li><a href="pages/inventaire.php">Inventaire</a></li>
+                          <li><a href="pages/gestion.php">Gestion</a></li>';
+                }
+
+                // Bouton déconnexion visible pour TOUT utilisateur connecté
+                if (isset($_SESSION['login'])) {
+                    echo '<li><a href="pages/logout.php">Déconnexion</a></li>';
+                }
+                ?>
+            </ul>
         </nav>
 
         <div class="contenu_index">
             <h1>Bienvenue, sur votre gestion de parc IT simplifiée</h1>
+
             <div class="video-container">
                 <video controls>
                     <source src="video/video_explication.mp4" type="video/mp4">
-                    Votre navigateur ne supporte pas la lecture de vidéo.
                 </video>
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda, consectetur delectus doloribus, eaque error id laudantium necessitatibus nostrum obcaecati placeat quo. Ea excepturi minus perspiciatis quo repellendus repudiandae, ut! Assumenda cum cupiditate eos ex iusto nulla, sunt. Aperiam aut nostrum provident qui sequi tempore unde veritatis voluptatem? Accusamus aliquam, architecto beatae debitis delectus deserunt dicta dolorum eligendi enim esse ex explicabo fuga hic ipsum iusto maxime neque nisi nobis nostrum numquam obcaecati odit officia provident quia quisquam quo repellat sit soluta sunt suscipit tempora tenetur totam, veritatis vitae voluptas voluptatem, voluptatibus. Ab alias aspernatur dicta ea enim iure quisquam. Animi cupiditate dolorem error expedita, maxime necessitatibus nisi nulla placeat qui, sint temporibus voluptatem? Accusamus harum iste modi perspiciatis sint unde, vel! Autem culpa debitis deleniti, hic in iste necessitatibus non nostrum, pariatur soluta vel velit? Assumenda atque illum incidunt ipsa maiores non recusandae totam voluptatem? Ab accusantium, at autem cupiditate error hic, ipsam molestias necessitatibus optio pariatur placeat possimus reiciendis repellat. Aliquid asperiores autem blanditiis deleniti facere fugiat impedit, magni natus quas, qui quibusdam quisquam quo, sunt. Aliquid at est ipsa molestias nulla! Illo iure, tenetur. Aliquam atque aut eligendi facilis possimus suscipit unde? Aliquam hic maiores provident repellendus tempora tempore! Ab deserunt eius ex officia qui quisquam saepe suscipit vitae! Ab aliquam asperiores atque aut autem consequuntur cupiditate dolor ducimus error eum explicabo facilis fuga in laboriosam modi natus nobis quae quaerat quibusdam quis quisquam quod quos, reprehenderit, repudiandae saepe similique sunt vel. Accusantium aliquam animi asperiores autem corporis culpa cumque, dicta doloribus enim excepturi exercitationem facilis harum illo ipsum labore minima molestiae nemo nihil non officiis praesentium provident quo rem repellendus saepe sapiente sit soluta! Accusamus aut cumque doloribus eligendi eveniet ex explicabo id ipsa ipsum minus necessitatibus, nemo nesciunt optio, quaerat quia quibusdam repudiandae temporibus voluptate.</p>
-            </div>
+
+            <p>...</p>
+        </div>
     </div>
+
     <footer>
         <p>&copy; 2025 - Projet SAE - Groupe X</p>
     </footer>
 </div>
-
-
-
 
 </body>
 </html>
