18d01d5942e10617d39dcd59225dd2a9a80b36e8
julienx-hub <julien.272737@gmail.com>
2025-11-29 23:18:10 +0100

    julien MATHIAS : quand on appuie sur modifier, désormais la modification s'effectue dans la base de données

--- src/pages/gestion.php
+++ src/pages/gestion.php
@@ -16,6 +16,81 @@ session_start();
         <?php include("navbar.php"); ?>
 
         <div class="contenu">
+            <?php
+            $connecte = mysqli_connect("localhost", "root", "", "rpiBD");
+            if (!$connecte) {
+                die("Erreur de connexion");
+            }
+            if (isset($_POST['modifier'])) {
+                $id = intval($_POST['modif_id']);
+                $res = mysqli_query($connecte, "SELECT * FROM inventaire WHERE id=$id");
+                $resultat = mysqli_fetch_assoc($res);
+
+                if ($resultat) {
+                    echo '<h2>Modifier unité centrale</h2>';
+                    echo '<form method="post" class="form-modif">';
+                    echo '<input type="hidden" name="id" value="'.$resultat['id'].'">';
+
+                    echo '<table>';
+                    echo '<tbody>';
+
+                    echo '<tr><td>NOM</td><td><input type="text" name="nom" value="'.$resultat['nom'].'"></td></tr>';
+                    echo '<tr><td>NB DE SERIE</td><td><input type="text" name="nb_serie" value="'.$resultat['nb_serie'].'"></td></tr>';
+                    echo '<tr><td>FABRICANT</td><td><input type="text" name="fabricant" value="'.$resultat['fabricant'].'"></td></tr>';
+                    echo '<tr><td>MODELE</td><td><input type="text" name="modele" value="'.$resultat['modele'].'"></td></tr>';
+                    echo '<tr><td>TYPE</td><td><input type="text" name="type" value="'.$resultat['type'].'"></td></tr>';
+                    echo '<tr><td>CPU</td><td><input type="text" name="cpu" value="'.$resultat['cpu'].'"></td></tr>';
+                    echo '<tr><td>RAM</td><td><input type="number" name="ram" value="'.$resultat['ram'].'"></td></tr>';
+                    echo '<tr><td>STOCKAGE</td><td><input type="number" name="disk" value="'.$resultat['disk'].'"></td></tr>';
+                    echo '<tr><td>OS</td><td><input type="text" name="os" value="'.$resultat['os'].'"></td></tr>';
+                    echo '<tr><td>DOMAINE</td><td><input type="text" name="domaine" value="'.$resultat['domaine'].'"></td></tr>';
+                    echo '<tr><td>LOCATION</td><td><input type="text" name="location" value="'.$resultat['location'].'"></td></tr>';
+                    echo '<tr><td>BATIMENT</td><td><input type="number" name="batiment" value="'.$resultat['batiment'].'"></td></tr>';
+                    echo '<tr><td>SALLE</td><td><input type="text" name="salle" value="'.$resultat['salle'].'"></td></tr>';
+                    echo '<tr><td>ETAT</td><td><input type="text" name="etat" value="'.$resultat['etat'].'"></td></tr>';
+
+
+                    echo '<tr><td colspan="2"><button type="submit" name="mise_à_jour" class="btn-valider">Modifier</button></td></tr>';
+                    echo '<tr><td colspan="2"><button><a href="gestion.php">Annuler</a></button></td></tr>';
+
+                    echo '</tbody>';
+                    echo '</table>';
+
+                    echo '</form>';
+                }
+            }
+            // quand le deuxième bouton est appuyé
+            if (isset($_POST['mise_a_jour'])) {
+                // Récupération des valeurs du formulaire
+                $id        = $_POST['modif_id'];
+                $nom       = $_POST['nom'];
+                $nb_serie  = $_POST['nb_serie'];
+                $fabricant = $_POST['fabricant'];
+                $modele    = $_POST['modele'];
+                $type      = $_POST['type'];
+                $cpu       = $_POST['cpu'];
+                $ram       = $_POST['ram'];
+                $disk      = $_POST['disk'];
+                $os        = $_POST['os'];
+                $domaine   = $_POST['domaine'];
+                $location  = $_POST['location'];
+                $batiment  = $_POST['batiment'];
+                $salle     = $_POST['salle'];
+                $etat      = $_POST['etat'];
+                $sql = "UPDATE inventaire 
+                SET nom=?, nb_serie=?, fabricant=?, modele=?, type=?, cpu=?, ram=?, disk=?, os=?, domaine=?, location=?, batiment=?, salle=?, etat=? 
+                WHERE id=?";
+
+                $stmt = mysqli_prepare($connecte, $sql);
+                mysqli_stmt_bind_param($stmt, "ssssssssssssssi",
+                        $nom, $nb_serie, $fabricant, $modele, $type, $cpu, $ram, $disk, $os, $domaine, $location, $batiment, $salle, $etat, $id);
+
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_close($stmt);
+
+                echo "Équipement mis à jour avec succès !";
+            }
+            ?>
             <h3>Liste de unité central : </h3>
             <div class="csv_box">
                 <label for="csvFile" class="csv">Importer CSV</label>
@@ -24,11 +99,6 @@ session_start();
             </div>
 
             <?php
-            $connecte = mysqli_connect("localhost", "root", "", "rpiBD");
-            if (!$connecte) {
-                die("Erreur de connexion");
-            }
-
             $data = mysqli_query($connecte, "SELECT * FROM inventaire");
             if (!$data) {
                 die("Erreur dans SELECT * ");
@@ -58,36 +128,45 @@ session_start();
                 </tr>
                 </thead>
                 <tbody>
-                <?php while ($ligne = mysqli_fetch_assoc($data)) : ?>
-                    <tr>
-                        <td>
-                            <form method='post' action='gestion.php'>
-                                <input type='hidden' name='suppr_id' value='<?= $ligne['id'] ?>'>
-                                <button type='submit' name='supprimer'>Supprimer</button>
-                            </form>
-                        </td>
-                        <td><?= $ligne['nom'] ?></td>
-                        <td><?= $ligne['nb_serie'] ?></td>
-                        <td><?= $ligne['fabricant'] ?></td>
-                        <td><?= $ligne['modele'] ?></td>
-                        <td><?= $ligne['type'] ?></td>
-                        <td><?= $ligne['cpu'] ?></td>
-                        <td><?= $ligne['ram'] ?></td>
-                        <td><?= $ligne['disk'] ?></td>
-                        <td><?= $ligne['os'] ?></td>
-                        <td><?= $ligne['domaine'] ?></td>
-                        <td><?= $ligne['location'] ?></td>
-                        <td><?= $ligne['batiment'] ?></td>
-                        <td><?= $ligne['salle'] ?></td>
-                        <td><?= $ligne['etat'] ?></td>
-                        <td>
-                            <form method='post' action='gestion_modifier.php'>
-                                <input type='hidden' name='modif_id' value='<?= $ligne['id'] ?>'>
-                                <button type='submit' name='modifier'>Modifier</button>
-                            </form>
-                        </td>
-                    </tr>
-                <?php endwhile; ?>
+                <?php
+                while ($ligne = mysqli_fetch_assoc($data)) {
+                    echo "<tr>";
+                    echo "<td>";
+                    echo "<form method='post' action='gestion.php'>";
+                    echo "<input type='hidden' name='suppr_id' value='" . $ligne['id'] . "'>";
+                    echo "<button type='submit' name='supprimer'>Supprimer</button>";
+                    echo "</form>";
+                    echo "</td>";
+
+                    echo "<td>" . $ligne['nom'] . "</td>";
+                    echo "<td>" . $ligne['nb_serie'] . "</td>";
+                    echo "<td>" . $ligne['fabricant'] . "</td>";
+                    echo "<td>" . $ligne['modele'] . "</td>";
+                    echo "<td>" . $ligne['type'] . "</td>";
+                    echo "<td>" . $ligne['cpu'] . "</td>";
+                    echo "<td>" . $ligne['ram'] . "</td>";
+                    echo "<td>" . $ligne['disk'] . "</td>";
+                    echo "<td>" . $ligne['os'] . "</td>";
+                    echo "<td>" . $ligne['domaine'] . "</td>";
+                    echo "<td>" . $ligne['location'] . "</td>";
+                    echo "<td>" . $ligne['batiment'] . "</td>";
+                    echo "<td>" . $ligne['salle'] . "</td>";
+                    echo "<td>" . $ligne['etat'] . "</td>";
+
+                    echo "<td>";
+                    echo "<form method='post'>";
+                    echo "<input type='hidden' name='modif_id' value='" . $ligne['id'] . "'>";
+                    echo "<button type='submit' name='modifier'>Modifier</button>";
+                    echo "</form>";
+                    echo "</td>";
+
+                    echo "</tr>";
+                }
+                ?>
+
+
+
+
                 </tbody>
             </table>
 
